88d0ef0fc7911552087aecf3a24b4451ce4df877
Omkar Yadav <httpsomkar@gmail.com>
2018-09-08 16:13:21 +0530

    Initial code

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('f4erp-root h1')).getText();
+  }
+}
